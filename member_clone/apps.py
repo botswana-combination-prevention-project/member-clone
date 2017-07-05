eb363f2f672d6f554f61937c40b7273253715389
erikvw <ew2789@gmail.com>
2017-07-05 11:40:36 +0200

    remove edc_map

--- member_clone/apps.py
+++ member_clone/apps.py
@@ -1,20 +1,5 @@
 from django.apps import AppConfig as DjangoApponfig
-from django.conf import settings
 
 
 class AppConfig(DjangoApponfig):
     name = 'member_clone'
-
-
-if settings.APP_NAME == 'member_clone':
-
-    from edc_map.apps import AppConfig as BaseEdcMapAppConfig
-
-    class EdcMapAppConfig(BaseEdcMapAppConfig):
-        verbose_name = 'Test Mappers'
-        mapper_model = 'plot.plot'
-        landmark_model = []
-        verify_point_on_save = False
-        zoom_levels = ['14', '15', '16', '17', '18']
-        identifier_field_attr = 'plot_identifier'
-        extra_filter_field_attr = 'enrolled'
